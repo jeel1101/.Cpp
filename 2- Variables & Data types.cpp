693d01a7fbe134c720f2d0c7caaa6758929d4cd7
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-25 10:50:12 +0530

    2- Variables & Data types.cpp

---
+++ 2- Variables & Data types.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    char ch= 'J';
+    float b= 3.14;
+    bool c= 0;
+    double d= 3132.12345678;
+    
+    cout<<"Integer is "<<a;
+    cout<<"\nCharacter is "<<ch;
+    cout<<"\nFloat is "<<b;
+    cout<<"\nBoolean is "<<c;
+    cout<<"\nDouble is "<<d;
+    
+    return 0;
+}
