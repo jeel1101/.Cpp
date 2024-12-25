8568d402a952b845c439c40ab0560baf4611065b
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-25 11:10:32 +0530

    Explicit typecasting.cpp

---
+++ 4- Explicit typecasting.cpp
@@ -0,0 +1,13 @@
+//explicit typecasting
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    double price= 500.99;
+    int newPrice= (int)price;
+    
+    cout<<newPrice;
+    
+    return 0;
+}
