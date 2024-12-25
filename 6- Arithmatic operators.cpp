096a44fb9c21cdf185460e403ca930d7ec501a20
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-25 11:35:23 +0530

    Arithmatic operators.cpp

---
+++ 6- Arithmatic operators.cpp
@@ -0,0 +1,14 @@
+//Arithmatic operators
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10,b=14;
+    
+    cout<<"Sum of "<<a<<" and "<<b<<" is "<<a+b;
+    cout<<"\nSub of "<<a<<" and "<<b<<" is "<<a-b;
+    cout<<"\nMul of "<<a<<" and "<<b<<" is "<<a*b;
+    cout<<"\nDiv of "<<a<<" and "<<b<<" is "<<a/b;
+    
+    return 0;
+}
