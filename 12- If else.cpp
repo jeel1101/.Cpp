4f9df21d6b3dedeb8b35cb1344eb1b132a856c0b
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-25 13:36:29 +0530

    If else.cpp

---
+++ 12- If else.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    
+    cout<<"Enter the number: ";
+    cin>>n;
+    
+    if(n>=0){
+        cout<<"\nGiven number is positive.";
+    }
+    else{
+        cout<<"\nGiven number is negative";
+    }
+    
+    return 0;
+}
