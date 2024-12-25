f0f84a7fde1a30dbb7f94b3d3f5c02d2f66d3926
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-25 11:49:38 +0530

    Logical operators.cpp

---
+++ 8- Logical operators.cpp
@@ -0,0 +1,11 @@
+//Logical operators
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<(3>1)||(1>3);
+    cout<<"\n"<<(3>1)&&(1>3);
+    cout<<"\n"<<!(6>4);
+    
+    return 0;
+}
