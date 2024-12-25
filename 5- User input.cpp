1fe723f4a74d3f74e00dedc5b74d229413433fc6
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-25 11:17:51 +0530

    User input.cpp

---
+++ 5- User input.cpp
@@ -0,0 +1,15 @@
+//user input
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    
+    cout<<"Enter your age: ";
+    cin>>age;
+    
+    cout<<"\nYou are "<<age<<" years old.";
+    
+    return 0;
+}
