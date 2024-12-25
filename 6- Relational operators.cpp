5202c9c9870dcb8ab3953089040ad5fffac469c9
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-25 11:43:24 +0530

    Relational operators.cpp

---
+++ 6- Relational operators.cpp
@@ -0,0 +1,12 @@
+//Relational operators
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<(3>5)<<"\n";
+    cout<<(3==3)<<"\n";
+    cout<<(3!=5)<<"\n";
+    cout<<(3!=3)<<"\n";
+    
+    return 0;
+}
