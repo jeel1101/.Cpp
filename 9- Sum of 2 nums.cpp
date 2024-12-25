2074db1c7859a3646ded43239586d817a8b139a6
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-25 11:56:54 +0530

    Sum of 2 nums.cpp

---
+++ 9- Sum of 2 nums.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    
+    cout<<"Enter the two numbers: ";
+    cin>>a>>b;
+    
+    sum= a+b;
+    cout<<"\n\nSum of "<<a<<" and "<<b<<" is "<<sum;
+    
+    return 0;
+}
