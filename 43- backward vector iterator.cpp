b42dfab64a6258e5394b230056d14a9873e55c2e
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-27 16:21:43 +0530

    backward vector iterator.cpp

---
+++ 43- backward vector iterator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+   vector<int>vec={1,2,3,4,5};
+   vector<int>::reverse_iterator it;
+   
+   for(auto it=vec.rbegin();it!=vec.rend();it++){
+       cout<<*(it)<<" ";
+   }
+    
+    return 0;
+}
