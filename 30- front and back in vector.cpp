84d3d950afe4a33ee86c256e37c76d24ddb07ff7
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-27 11:43:59 +0530

    front and back in vector.cpp

---
+++ 30- front and back in vector.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int>vec={11,14,2};
+    cout<<vec.front()<<endl;
+    cout<<vec.back();
+    
+    return 0;
+}
