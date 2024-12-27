daac671bea4604dadd203da6c8ac5000476e7c48
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-27 15:22:22 +0530

    input output of character array string.cpp

---
+++ 38- input output of character array string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char str[100];
+    
+    cout<<"Enter the string: ";
+    cin.getline(str,100);
+    
+    cout<<"\n\n"<<str;
+    
+    return 0;
+}
