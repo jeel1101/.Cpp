9eacf757a111ad36baeea5cbc7675c56858c6be9
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-27 12:31:29 +0530

    function declaration.cpp

---
+++ 32- function declaration.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printHello(){
+    cout<<"Hello"<<endl;
+}
+int main()
+{
+    printHello();   //function call
+    
+    return 0;
+}
