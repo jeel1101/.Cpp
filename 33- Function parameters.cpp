70b112ca300ff878c0b451cf6edeb957f8d2cb3f
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-27 12:56:18 +0530

    Function parameters.cpp

---
+++ 33- Function parameters.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//sum of 2 nums
+int sum(int a, int b){
+    return a+b;
+}
+int main(){
+    int a,b;
+    
+    cout<<"Enter the two numbers: ";
+    cin>>a>>b;
+    int s= sum(a,b);
+    cout<<"\n\nSum is: "<<s;
+    
+    return 0;
+}
