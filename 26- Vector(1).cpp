7ab4dd8064c72545ca4aa664200f2d833113ea7d
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-27 11:15:28 +0530

    Vector(1).cpp

---
+++ 26- Vector(1).cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int>vec={1,2,3};  //vecror of size 3
+    
+    cout<<vec[0]<<" "<<vec[1]<<" "<<vec[2];
+    
+    return 0;
+}
