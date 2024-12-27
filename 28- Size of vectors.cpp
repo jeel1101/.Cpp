513b69f0c1747dea60c06ff1b1bea20a5ca46d93
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-27 11:24:07 +0530

    Size of vectors.cpp

---
+++ 28- Size of vectors.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int>vec={'a','b','c','d','e'};  //vecror of size 3
+    
+    cout<<"Size of vector is: "<<vec.size();
+    
+    return 0;
+}
